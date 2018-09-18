0b9a11fb5d2c16b5dba6dbfa373c2721061b3725
Jodie <jodiemmuller@gmail.com>
2018-09-18 18:44:44 +1000

    Updated to work with new version

--- Collaboro/Collaboro/Collaboro/obj/Debug/Collaboro.App.xaml.g.cs
+++ Collaboro/Collaboro/Collaboro/obj/Debug/Collaboro.App.xaml.g.cs
@@ -11,7 +11,8 @@
 namespace Collaboro {
     
     
-    [global::Xamarin.Forms.Xaml.XamlFilePathAttribute("C:\\Users\\Thomas\\Documents\\GitHub\\JMVK\\Collaboro\\Collaboro\\Collaboro\\App.xaml")]
+    [global::Xamarin.Forms.Xaml.XamlFilePathAttribute("C:\\Users\\jodie\\Documents\\0 Documents\\University\\IAB330\\GitHub\\JMVK\\Collaboro\\Coll" +
+        "aboro\\Collaboro\\App.xaml")]
     public partial class App : global::Xamarin.Forms.Application {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
