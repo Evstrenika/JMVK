721dcbeccf6e56387de0c4b1337100c0e7c92012
kjurczyk <kjurczyk@ufl.edu>
2018-10-30 22:29:25 +1000

    worked on collaboro.tests

--- Collaboro/Collaboro/Collaboro.Tests.cs
+++ Collaboro/Collaboro/Collaboro.Tests.cs
@@ -68,6 +68,84 @@ namespace Collaboro
             app.WaitForElement(x => x.Marked("ResultLabel").Text("8"));
             Assert.Pass("Your first passing test");
         }
+
+
+        // name is left blank
+        [Test]
+        public void TestSignUpNameBlank()
+        {
+            // enter last name
+            app.Tap("surnameEntry");
+            app.EnterText("Doe");
+            app.Back();
+            // enter email address
+            app.Tap("emailAddressEntry");
+            app.EnterText("jason@gmail.com");
+            app.Back();
+            // enter double-check email address
+            app.Tap("confirmEmailAdressEntry");
+            app.EnterText("jason@gmail.com");
+            app.Back();
+            // enter password
+            app.Tap("passwordEntry");
+            app.EnterText("222222");
+            app.Back();
+            // enter double-check password
+            app.Tap("confirmPasswordEntry");
+            app.EnterText("222222");
+            app.Back();
+            // select university
+            app.Tap("universityPicker");
+            app.Tap("QUT");
+            // set switch to "accept"
+            app.Tap("acceptTC");
+
+            // check if the page went to the next page
+            // (or if it gave the user a message like it was supposed to)
+
+            Assert.Pass("Your first passing test");
+        }
+
+
+        // name is left blank
+        [Test]
+        public void TestSignUpSurnameBlank()
+        {
+            // enter first name
+            app.Tap("firstNameEntry");
+            app.EnterText("Jason");
+            app.Back();
+            // enter email address
+            app.Tap("emailAddressEntry");
+            app.EnterText("jason@gmail.com");
+            app.Back();
+            // enter double-check email address
+            app.Tap("confirmEmailAdressEntry");
+            app.EnterText("jason@gmail.com");
+            app.Back();
+            // enter password
+            app.Tap("passwordEntry");
+            app.EnterText("222222");
+            app.Back();
+            // enter double-check password
+            app.Tap("confirmPasswordEntry");
+            app.EnterText("222222");
+            app.Back();
+            // select university
+            app.Tap("universityPicker");
+            app.Tap("QUT");
+            // set switch to "accept"
+            app.Tap("acceptTC");
+
+            // check if the page went to the next page
+            // (or if it gave the user a message like it was supposed to)
+
+            Assert.Pass("Your first passing test");
+        }
+
+
+
+
     }
     
     
